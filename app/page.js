39269fdd8d9c65da517a300d1f3b57f46972422d
misasalam001-cloud <misasalam001@gmail.com>
2026-08-28 01:03:23 +0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Maracana</h1>
+      <p>Maracana tətbiqinə xoş gəlmisiniz.</p>
+
+      <a href="/login">Daxil ol</a>
+      <br />
+      <a href="/register">Qeydiyyatdan keç</a>
+    </main>
+  );
+}

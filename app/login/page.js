8ebc59a61d711b1bf22004160f003343eeb944bb
misasalam001-cloud <misasalam001@gmail.com>
2026-08-28 01:07:04 +0400

    app/login/page.js

---
+++ app/login/page.js
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    alert("Login sistemi hazırlanır...");
+  }
+
+  return (
+    <main>
+      <h1>Maracana</h1>
+      <h2>Daxil ol</h2>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Şifrə"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <br /><br />
+
+        <button type="submit">Daxil ol</button>
+      </form>
+
+      <br />
+
+      <a href="/register">Hesabın yoxdur? Qeydiyyatdan keç</a>
+    </main>
+  );
+}
